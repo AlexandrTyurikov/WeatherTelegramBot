c140f27a830ce69da9036661ec30e9a8de6ee317
AlexandrTyurikov <48610264+AlexandrTyurikov@users.noreply.github.com>
2020-01-22 01:41:52 +0300

    Update constants.py
    support_photo

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 api_token_bot = "Ваш API-токен бота"
 api_key_pyowm = "Ваш API-ключ openweathermap"
 api_key_yandex_geocoder = 'Ваш API-ключ яндекс-геокодер'
-support_photo_id = "AgADAgAD36sxG0zU4EvLu0ZRx9WLEfxAhA8ABAEAAwIAA3kAA8mzBQABFgQ"
+support_photo_url = "https://i.postimg.cc/bJBtN3zm/support-photo.jpg"
